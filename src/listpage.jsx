1819f7cd5e20dd947cc2cdad5a3faa929f8d7b4e
Mehul kumar <mehulkkr82@gmail.com>
2023-11-11 11:58:36 +0530

    edit handlesubmit1

--- src/listpage.jsx
+++ src/listpage.jsx
@@ -5,6 +5,7 @@ function Listpage(){
   const [arr,setarr]=useState([])
   const [btn,setbtn]=useState(true)
   const [ind,setind]=useState(-1)
+  const [nextbtn,setnextbtn]=useState(0)
   function load(){
   if(localStorage.getItem('array')){
     let val=JSON.parse(localStorage.getItem('array'))
@@ -50,11 +51,22 @@ function Listpage(){
     })
     localStorage.setItem('array',JSON.stringify(ans))
     setarr(ans)
+    if(nextbtn!=0)
+    setnextbtn(nextbtn-1)
     }
   }
   }
+  function handlenext(){
+    let val=JSON.parse(localStorage.getItem('array'))
+    let ans=[]
+    for(let i=3;i<val.length;i++){
+      ans.push(val[i]);
+    }
+    setarr(ans)
+  }
   function handlesubmit(){
         let val=[]
+        console.log(nextbtn)
         if(localStorage.getItem('array')){
           val=JSON.parse(localStorage.getItem('array'))
           val.unshift(name)
@@ -65,7 +77,7 @@ function Listpage(){
         localStorage.setItem('array',JSON.stringify(val))
         setarr(val)
         setname("")
-
+        setnextbtn(nextbtn+1)
   }
   
   
@@ -81,14 +93,23 @@ function Listpage(){
       )}
     </div>
     {arr.map((items,index)=>{
+      if(index<4){
       return(
+        <>
+        
         <div className='wrap-form'>
         <h2>{items}</h2>
         <button className="btn button5" onClick={()=>handledit(items,index)}>edit</button>
         <button className="btn button3" onClick={()=>handldel(index)}>delete</button>
         </div>
+        </>
        )
-    })}
+}})}
+    {nextbtn>=3 && (
+          <div>
+            <button className="btn button3" onClick={()=>handlenext()}>Next</button>
+          </div>
+    )}
   </div>
   )
 }
